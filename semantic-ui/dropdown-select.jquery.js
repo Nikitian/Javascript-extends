c7820a3c7f02515be87352994ef2e388fd26405d
Nikitian <mail@nikitian.ru>
2015-05-08 10:09:47 +0300

    Add semantic-ui/dropdown-select

---
+++ semantic-ui/dropdown-select.jquery.js
@@ -0,0 +1,22 @@
+jQuery.fn.extend({
+    /**
+     * Select item in semantic-ui/dropdown by value
+     * @param {mixed} value
+     * @returns {Boolean} If selected value not found in set, return false
+     * @see http://semantic-ui.com/modules/dropdown.html
+	 * @author <mail@nikitian.ru> Nikitian
+     */
+    dropdownSelect: function (value) {
+        $(".menu",this).data("selectedValue", value);
+        $(this).find(".menu>.item").each(function(index, el) {
+            if($(el).parent().data("selectedValue") == $(el).data("value")) {
+                $(this).parent().find(".item.active").removeClass("active selected");
+                $(el).addClass("active selected");
+                $(this).parent().parent().find("input[type='hidden']:first").val($(el).data("value"));
+                $(this).parent().prev(".text").empty().append($(el).html());
+            }
+        });
+        return ($("input[type='hidden']:first", this).val() == value);
+            
+    }
+});
